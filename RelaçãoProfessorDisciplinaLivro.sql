b40c5abe18b758d0ceaf1fd4f8dd97826253fd87
Alan Menescal Cáceres <66499569+amenesca@users.noreply.github.com>
2023-11-14 16:58:00 -0300

    Create RelaçãoProfessorDisciplinaLivro.sql

---
+++ RelaçãoProfessorDisciplinaLivro.sql
@@ -0,0 +1,25 @@
+create table disciplinas(
+	id_disciplina integer not null,
+	nome varchar(15) not null,
+	ementa not null,
+)
+
+create table professores(
+	id_professor integer not null,
+	celular varchar(15) not null,
+	nome varchar(40) not null,
+	id_disciplina integer not null,
+	primary key (id_professor),
+	foreign key (id_disciplina)
+	references disciplinas(id_disciplina)
+)
+
+create table livro(
+	id_livro integer not null,
+	nome varchar(15) not null,
+	autor varchar(40) not null,
+	sinopse varchar(500),
+	primary key (id_livro),
+	foreign key (id_disciplina)
+	references disciplinas(id_disciplina)
+)
